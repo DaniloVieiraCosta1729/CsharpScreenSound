888cda430f7c07c81d19c492a598d4bafe66b855
Danilo Vieria Costa <danilo.11235@outlook.com>
2023-11-04 18:35:39 -0300

    Primeiro exercício.

---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,14 @@
+﻿string mensagemDeBoasVindas = "Bem vindo ao Screen Sound";
+
+void ExibirMensagem(string mensagem)
+{
+    string confete = "*";
+    for (global::System.Int32 i = 0; i < 40; i++)
+    {
+        confete = confete + "*";
+    }
+
+    Console.WriteLine($"{confete}\n{mensagem}\n{confete}");
+}
+
+ExibirMensagem(mensagemDeBoasVindas);
